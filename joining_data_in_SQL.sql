15a0e691db09db572809c26bdc28080fece73ecd
Nick <73852660+NickP2007@users.noreply.github.com>
2021-12-10 19:34:56 -0800

    Update joining_data_in_SQL.sql

--- joining_data_in_SQL.sql
+++ joining_data_in_SQL.sql
@@ -438,3 +438,16 @@ LIMIT 5;
 --- SET THEORY CLAUSES
 
 --- Union
+
+-- Select fields from 2010 table
+SELECT *
+-- From 2010 table
+FROM economies2010
+-- Set theory clause
+UNION
+-- Select fields from 2015 table
+SELECT *
+-- From 2015 table
+FROM economies2015
+-- Order by code and year
+ORDER BY  code, year;
