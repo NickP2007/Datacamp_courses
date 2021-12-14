fc90a707dca7b642454f3c6dd0afdc9505760cac
Nick <73852660+NickP2007@users.noreply.github.com>
2021-12-13 22:05:23 -0800

    Update joining_data_in_SQL.sql

--- joining_data_in_SQL.sql
+++ joining_data_in_SQL.sql
@@ -569,3 +569,42 @@ INNER JOIN currencies AS c2
 ON c1.code = c2.code
 -- Where continent is Oceania
 WHERE continent = 'Oceania';
+
+-- Select fields
+SELECT code, name
+-- From Countries
+FROM countries
+-- Where continent is Oceania
+WHERE continent = 'Oceania'
+-- And code not in
+AND code NOT IN 
+-- Subquery
+(SELECT code
+FROM currencies);
+
+--- Set theory challenge
+
+-- Select the city name
+SELECT name
+-- Alias the table where city name resides
+FROM cities AS c1
+-- Choose only records matching the result of multiple set theory clauses
+WHERE country_code IN
+(
+   -- Select appropriate field from economies AS e
+   SELECT e.code
+   FROM economies AS e
+   -- Get all additional (unique) values of the field from currencies AS c2  
+   UNION
+   SELECT c2.code
+   FROM currencies AS c2
+   -- Exclude those appearing in populations AS p
+   EXCEPT
+   SELECT p.country_code
+   FROM populations AS p
+);
+
+--- SUBQUERIES
+
+--- Subquery inside where
+
