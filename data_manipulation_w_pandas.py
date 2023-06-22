f69b63947457b8357e6a956433bac8b3d4e6d22c
Nick <73852660+NickP2007@users.noreply.github.com>
2023-06-22 15:43:30 -0700

    Update data_manipulation_w_pandas.py

--- data_manipulation_w_pandas.py
+++ data_manipulation_w_pandas.py
@@ -434,3 +434,205 @@ nb_sold_by_size.plot(kind='bar')
 
 # Show the plot
 plt.show()
+
+
+Instructions: Changes in Sales Over Time
+# Get the total number of avocados sold on each date. The DataFrame has two rows for each date—one for organic, and one for conventional. Save this as nb_sold_by_date.
+# Create a line plot of the number of avocados sold.
+# Show the plot.
+
+# Import matplotlib.pyplot with alias plt
+import matplotlib.pyplot as plt
+
+# Get the total number of avocados sold on each date
+nb_sold_by_date = avocados.groupby('date')['nb_sold'].sum()
+
+# Create a line plot of the number of avocados sold by date
+nb_sold_by_date.plt(kind = line)
+
+# Show the plot
+plt.show()
+
+
+Instructions: Avocado Supply and Demand
+# Create a scatter plot with nb_sold on the x-axis and avg_price on the y-axis. Title it "Number of avocados sold vs. average price".
+# Show the plot.
+
+# Scatter plot of avg_price vs. nb_sold with title
+avocados.plot(x = 'nb_sold', y = 'avg_price', kind = 'scatter', title = 'Number of avocados sold vs. average price') 
+
+# Show the plot
+plt.show()
+
+
+Instructions: Price of Conventional vs. Organic Avocados
+# 1/3
+# Subset avocados for the conventional type, and the average price column. Create a histogram.
+# Create a histogram of avg_price for organic type avocados.
+# Add a legend to your plot, with the names "conventional" and "organic".
+# Show your plot.
+
+# Histogram of conventional avg_price 
+avocados[avocados["type"] == "conventional"]["avg_price"].hist()
+
+# Histogram of organic avg_price
+avocados[avocados["type"] == "organic"]["avg_price"].hist()
+
+# Add a legend
+plt.legend(["conventional", "organic"])
+
+# Show the plot
+plt.show()
+
+# 2/3
+# Modify your code to adjust the transparency of both histograms to 0.5 to see how much overlap there is between the two distributions.
+avocados[avocados["type"] == "conventional"]["avg_price"].hist(alpha = .5)
+
+# Modify histogram transparency to 0.5
+avocados[avocados["type"] == "organic"]["avg_price"].hist(alpha = .5)
+
+# 3/3
+# Modify your code to use 20 bins in both histograms.
+avocados[avocados["type"] == "conventional"]["avg_price"].hist(alpha = .5, bins = 20)
+avocados[avocados["type"] == "organic"]["avg_price"].hist(alpha = .5, bins = 20)
+
+
+Instructions: Finding Missing Values 
+# Print a DataFrame that shows whether each value in avocados_2016 is missing or not.
+# Print a summary that shows whether any value in each column is missing or not.
+# Create a bar plot of the total number of missing values in each column.
+
+# Import matplotlib.pyplot with alias plt
+import matplotlib.pyplot as plt
+
+# Check individual values for missing values
+print(avocados_2016.isna())
+
+# Check each column for missing values
+print(avocados_2016.isna().any())
+
+# Bar plot of missing values by variable
+avocados_2016.isna().sum().plot(kind = 'bar')
+
+# Show plot
+plt.show()
+
+
+Instructions: Removing Missing Values
+# Remove rows with missing values
+avocados_complete = avocados_2016.dropna()
+
+# Check if any columns contain missing values
+print(avocados_complete.isna().any())
+
+
+Instructions: Replacing Missing Values
+# 1/2
+# A list has been created, cols_with_missing, containing the names of columns with missing values: "small_sold", "large_sold", and "xl_sold".
+# Create a histogram of those columns.
+# Show the plot
+
+
+# List the columns with missing values
+cols_with_missing = ["small_sold", "large_sold", "xl_sold"]
+
+# Create histograms showing the distributions cols_with_missing
+avocados_2016[cols_with_missing].hist()
+
+# Show the plot
+plt.show()
+
+# 2/2
+# Replace the missing values of avocados_2016 with 0s and store the result as avocados_filled.
+# Create a histogram of the cols_with_missing columns of avocados_filled.
+
+# From previous step
+cols_with_missing = ["small_sold", "large_sold", "xl_sold"]
+avocados_2016[cols_with_missing].hist()
+plt.show()
+
+# Fill in missing values with 0
+avocados_filled = avocados_2016.fillna(0)
+
+# Create histograms of the filled columns
+avocados_filled[cols_with_missing].hist()
+
+# Show the plot
+plt.show()
+
+
+Instructions: List of Dictionaries
+# Create a list of dictionaries with new data
+avocados_list = [
+    {'date': "2019-11-03", 'small_sold': 10376832, 'large_sold': 7835071},
+    {'date': "2019-11-10", 'small_sold': 10717154, 'large_sold': 8561348},
+]
+
+# Convert list into DataFrame
+avocados_2019 = pd.DataFrame(avocados_list)
+
+# Print the new DataFrame
+print(avocados_2019)
+
+
+Instructions: Dictionary of Lists
+# Create a dictionary of lists with new data
+avocados_dict = {
+  "date": ["2019-11-17", "2019-12-01"],
+  "small_sold": [10859987, 9291631],
+  "large_sold": [7674135, 6238096]
+}
+
+# Convert dictionary into DataFrame
+avocados_2019 = pd.DataFrame(avocados_dict)
+
+# Print the new DataFrame
+print(avocados_2019)
+
+
+Instructions: CSV to DataFrame
+# 1/4
+# Read CSV as DataFrame called airline_bumping
+airline_bumping = pd.read_csv('airline_bumping.csv')
+
+# Take a look at the DataFrame
+print(airline_bumping.head())
+
+# 2/4
+# From previous step
+airline_bumping = pd.read_csv("airline_bumping.csv")
+print(airline_bumping.head())
+
+# For each airline, select nb_bumped and total_passengers and sum
+airline_totals = airline_bumping.groupby('airline')[['nb_bumped','total_passengers']].sum()
+
+# 3/4
+# From previous steps
+airline_bumping = pd.read_csv("airline_bumping.csv")
+print(airline_bumping.head())
+airline_totals = airline_bumping.groupby("airline")[["nb_bumped", "total_passengers"]].sum()
+
+# Create new col, bumps_per_10k: no. of bumps per 10k passengers for each airline
+airline_totals["bumps_per_10k"] = airline_totals['nb_bumped'] / airline_totals['total_passengers'] * 10000
+
+# 4/4
+# From previous steps
+airline_bumping = pd.read_csv("airline_bumping.csv")
+print(airline_bumping.head())
+airline_totals = airline_bumping.groupby("airline")[["nb_bumped", "total_passengers"]].sum()
+airline_totals["bumps_per_10k"] = airline_totals["nb_bumped"] / airline_totals["total_passengers"] * 10000
+
+# Print airline_totals
+print(airline_totals)
+
+
+Instructions: DataFrame to CSV
+# Create airline_totals_sorted
+airline_totals_sorted = airline_totals.sort_values('bumps_per_10k',ascending = False)
+
+# Print airline_totals_sorted
+print(airline_totals_sorted)
+
+# Save as airline_totals_sorted.csv
+airline_totals_sorted.to_csv('airline_totals_sorted.csv')
+
