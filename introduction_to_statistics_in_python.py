9d8b541445138f17459330b29bd2bc9b72dffb1b
Nick <73852660+NickP2007@users.noreply.github.com>
2023-07-13 15:10:54 -0700

    Update introduction_to_statistics_in_python.py

--- introduction_to_statistics_in_python.py
+++ introduction_to_statistics_in_python.py
@@ -15,3 +15,92 @@ print(np.median(be_consumption['consumption']))
 # Calculate mean and median consumption in USA
 print(np.mean(usa_consumption['consumption']))
 print(np.median(usa_consumption['consumption']))
+
+# Import numpy as np
+import numpy as np
+
+# Subset for Belgium and USA only
+be_and_usa = food_consumption[(food_consumption['country']=='Belgium') | (food_consumption['country']=='USA')]
+
+# Group by country, select consumption column, and compute mean and median
+print(be_and_usa.groupby('country')['consumption'].agg([np.mean,np.median]))
+
+# MEAN VS MEDIAN
+# Import matplotlib.pyplot with alias plt
+import matplotlib.pyplot as plt
+
+# Subset for food_category equals rice
+rice_consumption = food_consumption[food_consumption['food_category']=='rice']
+
+# Histogram of co2_emission for rice and show plot
+rice_consumption['co2_emission'].hist()
+plt.show()
+
+# Subset for food_category equals rice
+rice_consumption = food_consumption[food_consumption['food_category'] == 'rice']
+
+# Calculate mean and median of co2_emission with .agg()
+print(rice_consumption['co2_emission'].agg([np.mean,np.median]))
+
+# QUARTILES, QUANTILES, & QUINTILES
+# Calculate the quartiles of co2_emission
+print(np.quantile(food_consumption['co2_emission'],[0,0.25,0.5,0.75,1]))
+
+# Calculate the quintiles of co2_emission
+print(np.quantile(food_consumption['co2_emission'],[0, 0.2, 0.4, 0.6, 0.8, 1]))
+
+# Calculate the deciles of co2_emission
+print(np.quantile(food_consumption['co2_emission'],np.linspace(0, 1, 11)))
+
+# VARIANCE AND STANDARD DEVIATION
+# Print variance and sd of co2_emission for each food_category
+print(food_consumption.groupby('food_category')['co2_emission'].agg([np.var,np.std]))
+
+# Import matplotlib.pyplot with alias plt
+import matplotlib.pyplot as plt
+
+# Create histogram of co2_emission for food_category 'beef'
+food_consumption[food_consumption['food_category']=='beef']['co2_emission'].hist()
+# Show plot
+plt.show()
+
+# Create histogram of co2_emission for food_category 'eggs'
+food_consumption[food_consumption['food_category']=='eggs']['co2_emission'].hist()
+# Show plot
+plt.show()
+
+# FINDING OUTLIERS USING IQR
+# Calculate total co2_emission per country: emissions_by_country
+emissions_by_country = food_consumption.groupby('country')['co2_emission'].sum()
+
+# Compute the first and third quantiles and IQR of emissions_by_country
+q1 = np.quantile(emissions_by_country, 0.25)
+q3 = np.quantile(emissions_by_country, 0.75)
+iqr = q3 - q1
+
+# Calculate the lower and upper cutoffs for outliers
+lower = q1 - 1.5 * iqr
+upper = q3 + 1.5 * iqr
+
+# Subset emissions_by_country to find outliers
+outliers = emissions_by_country[(emissions_by_country>upper) | (emissions_by_country<lower)]
+print(outliers)
+
+# CALCULATING PROBABILITIES
+# Count the deals for each product
+counts = amir_deals['product'].value_counts()
+
+# Calculate probability of picking a deal with each product
+probs = counts / amir_deals.shape[0]
+print(probs)
+
+# SAMPLING DEALS
+# Set random seed
+np.random.seed(24)
+
+# Sample 5 deals without replacement
+sample_with_replacement = amir_deals.sample(5, replace = True)
+print(sample_with_replacement)
+
+
+
