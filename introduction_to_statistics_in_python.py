3ae6ee761ddbea67a7f69becd64db30213db746f
Nick <73852660+NickP2007@users.noreply.github.com>
2023-07-14 16:04:00 -0700

    Update introduction_to_statistics_in_python.py

--- introduction_to_statistics_in_python.py
+++ introduction_to_statistics_in_python.py
@@ -102,5 +102,190 @@ np.random.seed(24)
 sample_with_replacement = amir_deals.sample(5, replace = True)
 print(sample_with_replacement)
 
+# CREATING A PROBABILITY DISTRIBUTION
+# Create probability distribution
+size_dist = restaurant_groups['group_size'].value_counts() / restaurant_groups.shape[0]
+# Reset index and rename columns
+size_dist = size_dist.reset_index()
+size_dist.columns = ['group_size', 'prob']
 
+# Expected value
+expected_value = np.sum(size_dist['group_size'] * size_dist['prob'])
 
+# Subset groups of size 4 or more
+groups_4_or_more = size_dist[size_dist['group_size']>=4]
+
+# Sum the probabilities of groups_4_or_more
+prob_4_or_more = np.sum(groups_4_or_more['prob'])
+print(prob_4_or_more)
+
+# DATA BACK-UPS
+# Min and max wait times for back-up that happens every 30 min
+min_time = 0
+max_time = 30
+
+# Import uniform from scipy.stats
+from scipy.stats import uniform
+
+# Calculate probability of waiting 10-20 mins
+prob_between_10_and_20 = uniform.cdf(20,min_time,max_time) - uniform.cdf(10,min_time,max_time)
+print(prob_between_10_and_20)
+
+# SIMULATING WAIT TIMES
+# Set random seed to 334
+np.random.seed(334)
+
+# Import uniform
+from scipy.stats import uniform
+
+# Generate 1000 wait times between 0 and 30 mins
+wait_times = uniform.rvs(0, 30, size=1000)
+
+# Create a histogram of simulated times and show plot
+plt.hist(wait_times)
+plt.show()
+
+# SIMULATING SALES DEALS
+# Import binom from scipy.stats
+from scipy.stats import binom
+
+# Set random seed to 10
+np.random.seed(10)
+
+# Simulate 52 weeks of 3 deals
+deals = binom.rvs(3,.3,size=52)
+
+# Print mean deals won per week
+print(np.mean(deals))
+
+# CALCULATING BINOMIAL PROBABILITIES
+# Probability of closing > 1 deal out of 3 deals
+prob_greater_than_1 = 1 - binom.cdf(1,3,.3)
+
+print(prob_greater_than_1)
+
+# HOW MANY SALES WILL BE WON?
+# Expected number won with 30% win rate
+won_30pct = 3 * .3
+print(won_30pct)
+
+# Expected number won with 25% win rate
+won_25pct = 3 * .25
+print(won_25pct)
+
+# Expected number won with 35% win rate
+won_35pct = 3 * .35
+print(won_35pct)
+
+# PROBABILITIES FROM THE NORMAL DISTRIBUTION
+# Calculate amount that 25% of deals will be less than
+pct_25 = norm.ppf(.25,5000,2000)
+
+print(pct_25)
+
+# SIMULATING SALES UNDER NEW MARKET CONDITIONS 
+# Calculate new average amount
+new_mean = 5000 * 1.2
+
+# Calculate new standard deviation
+new_sd = 2000 * 1.3
+
+# Simulate 36 new sales
+new_sales = norm.rvs(new_mean,new_sd,size=36)
+
+# Create histogram and show
+plt.hist(new_sales)
+plt.show()
+
+# THE CLT IN ACTION
+# Set seed to 104
+np.random.seed(104)
+
+sample_means = []
+# Loop 100 times
+for i in range(100):
+  # Take sample of 20 num_users
+  samp_20 = amir_deals['num_users'].sample(20, replace=True)
+  # Calculate mean of samp_20
+  samp_20_mean = np.mean(samp_20)
+  # Append samp_20_mean to sample_means
+  sample_means.append(samp_20_mean)
+  
+# Convert to Series and plot histogram
+sample_means_series = pd.Series(sample_means)
+sample_means_series.hist()
+# Show plot
+plt.show()
+
+# THE MEAN OF MEANS
+# Set seed to 321
+np.random.seed(321)
+
+sample_means = []
+# Loop 30 times to take 30 means
+for i in range(30):
+  # Take sample of size 20 from num_users col of all_deals with replacement
+  cur_sample = all_deals['num_users'].sample(20,replace=True)
+  # Take mean of cur_sample
+  cur_mean = np.mean(cur_sample)
+  # Append cur_mean to sample_means
+  sample_means.append(cur_mean)
+
+# Print mean of sample_means
+print(np.mean(sample_means))
+# Print mean of num_users in amir_deals
+print(np.mean(amir_deals['num_users']))
+
+# TRACKING LEAD RESPONSES
+# Import poisson from scipy.stats
+from scipy.stats import poisson
+
+# Probability of > 10 responses
+prob_over_10 = 1 - poisson.cdf(10,4)
+
+print(prob_over_10)
+
+# RELATIONSHIPS BETWEEN VARIABLES
+# Create scatterplot of happiness_score vs life_exp with trendline
+sns.lmplot(x='life_exp', y='happiness_score', data=world_happiness, ci=None)
+
+# Show plot
+plt.show()
+
+# Correlation between life_exp and happiness_score
+cor = world_happiness['life_exp'].corr(world_happiness['happiness_score'])
+
+print(cor)
+
+# WHAT CANT CORRELATION MEASURE
+# Scatterplot of gdp_per_cap and life_exp
+sns.scatterplot(x='gdp_per_cap', y='life_exp', data=world_happiness)
+
+# Show plot
+plt.show()
+  
+# Correlation between gdp_per_cap and life_exp
+cor = world_happiness['gdp_per_cap'].corr(world_happiness['life_exp'])
+
+print(cor)
+
+# TRANSFORMING VARIABLES
+# Create log_gdp_per_cap column
+world_happiness['log_gdp_per_cap'] = np.log(world_happiness['gdp_per_cap'])
+
+# Scatterplot of happiness_score vs. log_gdp_per_cap
+sns.scatterplot(x='log_gdp_per_cap',y='happiness_score',data=world_happiness)
+plt.show()
+
+# Calculate correlation
+cor = world_happiness['log_gdp_per_cap'].corr(world_happiness['happiness_score'])
+print(cor)
+
+# DOES SUGAR IMPROVE HAPPINESS
+# Scatterplot of grams_sugar_per_day and happiness_score
+sns.scatterplot(x='grams_sugar_per_day',y='happiness_score',data=world_happiness)
+plt.show()
+
+# Correlation between grams_sugar_per_day and happiness_score
+cor = world_happiness['grams_sugar_per_day'].corr(world_happiness['happiness_score'])
+print(cor)
